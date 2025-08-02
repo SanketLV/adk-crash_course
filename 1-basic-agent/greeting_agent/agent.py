af54c852b0fa268b781301439526f8013e1f0825
SanketLV <sanketlakhani80@gmail.com>
2025-08-02 14:16:11 +0530

    Add initial project structure with .gitignore, requirements, and greeting agent

---
+++ 1-basic-agent/greeting_agent/agent.py
@@ -0,0 +1,10 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="greeting_agent",
+    model="gemini-2.0-flash",
+    description="Greeting agent",
+    instruction="""
+    You are a helpful assistant that greets the user. Ask for user's name and greet then by name.
+  """,
+)
